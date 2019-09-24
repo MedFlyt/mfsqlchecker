8aff1cf640339059823f5a9db1e5223d369dac1b
benny-medflyt <benny@medflyt.com>
2019-09-24 14:29:07 +0300

    view name overflow checking

--- mfsqlchecker/view_names.ts
+++ mfsqlchecker/view_names.ts
@@ -1,12 +1,22 @@
 import * as crypto from "crypto";
 
+// <https://www.postgresql.org/docs/current/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS>
+const POSTGRES_IDENT_MAX_LEN = 63;
+
 export function calcViewName(varName: string | null, query: string) {
     const hash = crypto.createHash("sha1").update(query).digest("hex");
 
     const prefix = "$$mfv_";
-    const viewName = varName !== null
-        ? prefix + varName + "_" + hash.slice(0, 12)
-        : prefix + hash.slice(0, 12);
 
-    return viewName;
+    if (varName === null) {
+        return prefix + hash.slice(0, 12);
+    }
+
+    const overflow = prefix.length + varName.length + 1 + 12 - POSTGRES_IDENT_MAX_LEN;
+
+    const varName2 = overflow > 0
+        ? varName.substr(0, varName.length - overflow)
+        : varName;
+
+    return prefix + varName2 + "_" + hash.slice(0, 12);
 }
