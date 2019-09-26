27c85dfb3ee58152b5bd402200f395f246fcb4ef
benny-medflyt <benny@medflyt.com>
2019-09-27 00:31:58 +0300

    Add support for "float4" and "float8" types

--- mfsqlchecker/DbConnector.ts
+++ mfsqlchecker/DbConnector.ts
@@ -923,6 +923,9 @@ function sqlTypeToTypeScriptType(uniqueColumnTypes: Map<SqlType, TypeScriptType>
             return TypeScriptType.wrap("string");
         case "bool":
             return TypeScriptType.wrap("boolean");
+        case "float4":
+        case "float8":
+            return TypeScriptType.wrap("number");
 
         // TODO Temporary
         case "jsonb":
