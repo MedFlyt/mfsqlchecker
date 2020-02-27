470fc046c89e057b21b3abe9fc8ae50f258889f9
benny-medflyt <benny@medflyt.com>
2020-02-27 09:52:46 -0500

    Version 0.0.20

--- mfsqlchecker/main.ts
+++ mfsqlchecker/main.ts
@@ -56,7 +56,7 @@ function parseFormat(value: string): Format {
 
 function parseOptions(): Options {
     const program = new commander.Command();
-    program.version("0.0.19");
+    program.version("0.0.20");
 
     program
         .option("-w, --watch", "watch mode")
